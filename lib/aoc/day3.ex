6ef8cee3750b103e625a806b9e9ec4762510c802
Po Chen <chenpaul914@gmail.com>
2017-09-28 14:12:04 +1000

    add day 3 part 2

--- lib/aoc/day3.ex
+++ lib/aoc/day3.ex
@@ -2,16 +2,22 @@ defmodule AoC.Day3 do
   def solve do
     input = AoC.Util.fetch_input(3)
 
-    input
-    |> String.split("\n")
+    nums = String.split(input)
+
+    [
+      Stream.take_every(nums, 3),
+      Stream.take_every(Enum.drop(nums, 1), 3),
+      Stream.take_every(Enum.drop(nums, 2), 3),
+    ]
+    |> Stream.concat
+    |> Stream.chunk(3)
     |> Enum.count(&is_triangle?/1)
     |> IO.inspect
   end
 
-  defp is_triangle?(str) do
+  defp is_triangle?(strs) do
     [a, b, c] =
-      str
-      |> String.split
+      strs
       |> Enum.map(&String.to_integer/1)
       |> Enum.sort
 

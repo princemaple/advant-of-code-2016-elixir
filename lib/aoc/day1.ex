27b0962b340d275b88086085b0aca9e20256e9b6
Po Chen <chenpaul914@gmail.com>
2017-09-28 13:05:12 +1000

    add day 1 part 2

--- lib/aoc/day1.ex
+++ lib/aoc/day1.ex
@@ -2,17 +2,23 @@ defmodule AoC.Day1 do
   def solve do
     input = AoC.Util.fetch_input(1)
     commands = String.split(input, ", ")
-    {x, y} = do_solve(commands, :north, {0, 0})
+    {x, y} = do_solve(commands, :north, {0, 0}, MapSet.new)
 
     [x, y] |> Enum.map(&abs/1) |> Enum.sum |> IO.inspect
   end
 
-  defp do_solve([], _direction, coordinates), do: coordinates
-  defp do_solve([c | commands], direction, coordinates) do
+  defp do_solve([], _direction, coordinates, _history), do: coordinates
+  defp do_solve([c | commands], direction, coordinates, history) do
     {turn_where, steps} = break_command(c)
     direction = turn(direction, turn_where)
-    coordinates = walk(coordinates, vectorize(direction, steps))
-    do_solve(commands, direction, coordinates)
+    {coordinates, history, visited} =
+      walk(coordinates, vectorize(direction, steps), history)
+
+    if visited do
+      visited
+    else
+      do_solve(commands, direction, coordinates, history)
+    end
   end
 
   defp break_command("L" <> steps), do: {:left, String.to_integer(steps)}
@@ -35,5 +41,21 @@ defmodule AoC.Day1 do
   defp vectorize(:east, n), do: {n, 0}
   defp vectorize(:west, n), do: {-n, 0}
 
-  defp walk({x, y}, {dx, dy}), do: {x + dx, y + dy}
+  defp walk(coordinates, {0, 0}, history),
+    do: {coordinates, history, nil}
+  defp walk(coordinates, distance, history) do
+    {coordinates, distance} = do_walk(coordinates, distance)
+
+    if MapSet.member?(history, coordinates) do
+      {coordinates, history, coordinates}
+    else
+      history = MapSet.put(history, coordinates)
+      walk(coordinates, distance, history)
+    end
+  end
+
+  defp do_walk({x, y}, {dx, 0}) when dx > 0, do: {{x + 1, y}, {dx - 1, 0}}
+  defp do_walk({x, y}, {dx, 0}) when dx < 0, do: {{x - 1, y}, {dx + 1, 0}}
+  defp do_walk({x, y}, {0, dy}) when dy > 0, do: {{x, y + 1}, {0, dy - 1}}
+  defp do_walk({x, y}, {0, dy}) when dy < 0, do: {{x, y - 1}, {0, dy + 1}}
 end
